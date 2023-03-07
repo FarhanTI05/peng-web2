4bcab62d1d38374c324f6a78493f7ea6728f6e51
FarhanTI05 <115351460+FarhanTI05@users.noreply.github.com>
2023-03-07 18:54:39 +0700

    Add files via upload

---
+++ praktikum2/index2.php
@@ -0,0 +1,21 @@
+<?php
+
+$_nama = $_GET['nama']
+$_matkul = $_GET['matkul']
+$_uts = $_GET["uts"]
+$_uas = $_GET["uas"]
+$_tugas = $_GET["tugas"]
+
+
+
+echo ' <br> Nama Mahasiswa:' . $_nama;
+echo '<br> Mata Kulia:' . $_matkul;
+echo '<br> Nilai Uts:' . $_uts;
+echo '<br> Nilai Uas :' . $_uas;
+echo '<br> Nilai Tugas :' $_tugas;
+
+
+
+
+
+?>
